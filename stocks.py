19e67e9b89fca99dcd22a64b9c17197c4297cb44
Kayla Brewer <cooper.kaylam@gmail.com>
2017-04-11 11:44:08 -0500

    added readme and fixed folder path.

---
+++ stocks.py
@@ -0,0 +1,26 @@
+stockDict = {
+    "GM": "General Motors",
+    "CAT": "Caterpillar",
+    "EK": "Eastman Kodak",
+    "APPL": "Apple Inc"
+    }
+
+purchases = [('GM', 100, '10-sep-2001', 48),
+             ('CAT', 100, '10-apr-1999', 24),
+             ('GM', 200, '1-jul-1998', 56),
+             ('APPL', 150, '1-aug-1998', 78),
+             ('CAT', 75, '17-oct-2002', 24),
+             ('EK', 45, '1-aug-1998', 65)]
+
+for purchase in purchases:
+    ticker_key = purchase[0]
+    shares_purchased = purchase[1]
+    purchase_price = purchase[3]
+    purchase_date = purchase[2]
+    total_price = shares_purchased * purchase_price
+    company_name = stockDict[ticker_key]
+    print("On {}, I purchased {} shares of {} stock for ${}".format(purchase_date, shares_purchased, company_name, total_price)+".")
+
+### TODO need to create a dictionary where the keys are the ticker_key and the values are the total number of stocks purchased.
+
+for purchase in purchases:
